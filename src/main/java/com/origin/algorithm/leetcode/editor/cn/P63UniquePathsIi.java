d41fa531363e11e620239bd2879e487e1b8ada9b
gudaoge <>
2020-07-20 18:30:01 +0800

    力扣63

---
+++ src/main/java/com/origin/algorithm/leetcode/editor/cn/P63UniquePathsIi.java
@@ -0,0 +1,70 @@
+/*
+[63]不同路径 II
+unique-paths-ii
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。 
+//
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。 
+//
+// 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？ 
+//
+// 
+//
+// 网格中的障碍物和空位置分别用 1 和 0 来表示。 
+//
+// 说明：m 和 n 的值均不超过 100。 
+//
+// 示例 1: 
+//
+// 输入:
+//[
+//  [0,0,0],
+//  [0,1,0],
+//  [0,0,0]
+//]
+//输出: 2
+//解释:
+//3x3 网格的正中间有一个障碍物。
+//从左上角到右下角一共有 2 条不同的路径：
+//1. 向右 -> 向右 -> 向下 -> 向下
+//2. 向下 -> 向下 -> 向右 -> 向右
+// 
+// Related Topics 数组 动态规划
+
+*/
+package com.origin.algorithm.leetcode.editor.cn;
+public class P63UniquePathsIi {
+    public static void main(String[] args) {
+        //Solution solution = new P63UniquePathsIi().new Solution();
+    }
+
+    /**
+     * 思路：
+     * 跟前一道题差不多
+     * 多一步判断是否是障碍物的判断
+     */
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        dp[0][0] = 1;
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            for (int j = 0; j < obstacleGrid[0].length; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i - 1 >= 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
