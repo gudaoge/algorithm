831f336eb4ee85e7f42471b8e6aca9707409b17c
gudaoge <>
2020-11-27 14:15:36 +0800

    总结

---
+++ src/main/java/com/origin/algorithm/leetcode/editor/cn/structure/NestedInteger.java
@@ -0,0 +1,15 @@
+package com.origin.algorithm.leetcode.editor.cn.structure;
+
+import java.util.List;
+
+/**
+ * Created by dengqingling on 2020-08-12
+ */
+public interface NestedInteger {
+
+    boolean isInteger();
+
+    Integer getInteger();
+
+    List<NestedInteger> getList();
+}
